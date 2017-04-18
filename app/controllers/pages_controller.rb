933c78f13adbe0a81ee2f0c2a686b3075044e6b7
Alex McFarlane <email.mcfarlane@gmail.com>
2017-04-18 19:38:57 +0000

    Project completed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,13 @@ require 'twilio-ruby'
 
 class PagesController < ApplicationController
   def home
+    
   end
 
   def sms
 @client = Twilio::REST::Client.new ENV["twilio_sid"], ENV["twilio_auth_token"]
 @client.account.messages.create(:body => params["msg"],
-    :to => params["send"],
+    :to => params["receiving_number"],
     :from => ENV["from_number"])
 
   end
